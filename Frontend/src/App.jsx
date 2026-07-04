d7c7e3d3c4edde37077ccbecb7745bf0b7dcd160
BSV22 <basavakirandeshmukh@gmail.com>
2026-07-04 10:41:45 +0530

    Basic UI finished

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,20 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import Navbar from './components/navbar'
+import Footer from './components/footer'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <nav>
-        <li className='flex bg-blue-900'>
-          <ul>home</ul>
-          <ul>About</ul>
-          <ul>Contact</ul>
-        </li>
-      </nav>
-    </>
+   <>
+  <Navbar />
+
+  <main className="min-h-lvh bg-gray-900 flex items-center justify-center px-4">
+    <div className="w-full max-w-4xl">
+      <div className="flex items-center gap-3 bg-gray-700 p-3 rounded-2xl">
+        <input
+          type="text"
+          placeholder="Ask..."
+          className="flex-1 bg-transparent text-white placeholder-gray-400 outline-none px-3 py-2"
+        />
+
+        <button className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-2 rounded-xl">
+          Send
+        </button>
+      </div>
+    </div>
+  </main>
+  <Footer />
+</>
   )
 }
 
---
+++ Frontend/src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+       <div className="fixed top-0 left-0 w-full z-50 px-4 py-2">
+  <nav className="bg-blue-900 rounded-2xl px-6 py-3">
+    <div className="flex items-center justify-between">
+      <h1 className="text-white font-bold text-xl cursor-pointer">
+        Documind
+      </h1>
+
+      <ul className="flex items-center gap-8 text-white">
+        <li className="cursor-pointer hover:text-gray-300">Home</li>
+        <li className="cursor-pointer hover:text-gray-300">About</li>
+        <li className="cursor-pointer hover:text-gray-300">Contact</li>
+      </ul>
+    </div>
+  </nav>
+</div>
+    )
+}
+
+export default navbar
